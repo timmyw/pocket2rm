a8a18e0bb2af6fad1ed944bdc6b3fd641c0af276
Tim Whelan <tim.whelan@ravelin.com>
2018-12-13 20:54:14 +0000

    Added info links to readme

--- remarkable.go
+++ remarkable.go
@@ -2,6 +2,24 @@ package pocket2rm
 
 // RMToken is a reMarkable access token
 type RMToken struct {
-	Token  string `json:"token"`
+	Token string `json:"token"`
 }
 
+// GetRMToken will retrieve an access token from Pocket
+func GetRMToken(code string, deviceDesc string, deviceID string) (*RMToken, error) {
+	result := &RMToken{}
+	err := PostJSON("/token/json/2/device/new",
+		APIOriginRemarkable,
+		map[string]string{
+			"code":       code,
+			"deviceDesc": deviceDesc,
+			"deviceID":   deviceID,
+		},
+		result)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
